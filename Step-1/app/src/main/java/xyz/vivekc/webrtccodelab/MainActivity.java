d29db260e4bd82d744469b98a66f494a8b1d72f7
ilkayaktas <ilkayaktas@gmail.com>
2020-08-16 10:28:23 +0300

    Application name is updated. New comments are added. Gradle version is updated.

--- Step-1/app/src/main/java/xyz/vivekc/webrtccodelab/MainActivity.java
+++ Step-1/app/src/main/java/xyz/vivekc/webrtccodelab/MainActivity.java
@@ -16,6 +16,9 @@ import org.webrtc.VideoCapturer;
 import org.webrtc.VideoRenderer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
+import xyz.vivekc.webrtccodelab.R;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -28,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
         //Initialize PeerConnectionFactory globals.
         //Params are context, initAudio,initVideo and videoCodecHwAcceleration
         //PeerConnectionFactory.initializeAndroidGlobals(this, true, true, true);
-        PeerConnectionFactory.initialize(PeerConnectionFactory.InitializationOptions.builder(this).setEnableVideoHwAcceleration(true).createInitializationOptions());
-
+        PeerConnectionFactory.InitializationOptions options = PeerConnectionFactory.InitializationOptions.builder(this).setEnableVideoHwAcceleration(true).createInitializationOptions();
+        PeerConnectionFactory.initialize(options);
 
         //Create a new PeerConnectionFactory instance.
         //PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
@@ -38,30 +41,33 @@ public class MainActivity extends AppCompatActivity {
 
         //Now create a VideoCapturer instance. Callback methods are there if you want to do something! Duh!
         VideoCapturer videoCapturerAndroid = createVideoCapturer();
-        //Create MediaConstraints - Will be useful for specifying video and audio constraints. More on this later!
-        MediaConstraints constraints = new MediaConstraints();
 
         //Create a VideoSource instance
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid);
         VideoTrack localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
 
+        // We now have a VideoTrack which gives the stream of data from the device’s camera.
+
+        //Create MediaConstraints - Will be useful for specifying video and audio constraints. More on this later!
+        MediaConstraints constraints = new MediaConstraints();
+
         //create an AudioSource instance
         AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
         AudioTrack localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
 
-        //we will start capturing the video from the camera
-        //width,height and fps
+        //we will start capturing the video from the camera width,height and fps
         videoCapturerAndroid.startCapture(1000, 1000, 30);
 
+        // SurfaceViewRenderer displays video stream on screen
         //create surface renderer, init it and add the renderer to the track
-        SurfaceViewRenderer videoView = (SurfaceViewRenderer) findViewById(R.id.surface_rendeer);
+        SurfaceViewRenderer videoView = findViewById(R.id.surface_rendeer);
         videoView.setMirror(true);
 
         EglBase rootEglBase = EglBase.create();
         videoView.init(rootEglBase.getEglBaseContext(), null);
 
+        //Add the renderer to the video track
         localVideoTrack.addRenderer(new VideoRenderer(videoView));
-
 
     }
 
@@ -74,12 +80,14 @@ public class MainActivity extends AppCompatActivity {
         return videoCapturer;
     }
 
+    // Create a VideoCapturer instance which uses the camera of the device
     private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
         final String[] deviceNames = enumerator.getDeviceNames();
 
         // First, try to find front facing camera
         Logging.d(TAG, "Looking for front facing cameras.");
         for (String deviceName : deviceNames) {
+            Logging.d(TAG, "Device name " + deviceName);
             if (enumerator.isFrontFacing(deviceName)) {
                 Logging.d(TAG, "Creating front facing camera capturer.");
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
